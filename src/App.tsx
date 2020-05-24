85fd2f339d26882db832d027fa0868165eb88c7d
Maksym Borysenko <mborysenko@sdl.com>
2020-05-24 10:06:48 +0200

    *Added middleware for using Redux Development Tools & Fixed connected router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 import '@explorer/routing';
 
 import * as React from 'react';
-import { Router } from 'react-router';
+import { ConnectedRouter } from 'connected-react-router';
 import { Route, Switch } from 'react-router-dom';
 
 import { useRootRouting } from '@common/routing';
-import { history } from '@common/store';
+import { history, store } from '@common/store';
+import { Provider } from 'react-redux';
+import { Column, LayoutList } from '@common';
 
 export type ApplicationProps = {};
 
@@ -14,13 +16,15 @@ export const App: React.FunctionComponent<ApplicationProps> = () => {
     const routes = useRootRouting();
 
     return (
-        <Router history={history}>
-            <Switch>
-                {routes.map(props => {
-                    return <Route key={props.path as string} {...props} />;
-                })}
-                <Route path="*" component={() => <div>404. Page not found.</div>} />
-            </Switch>
-        </Router>
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <Switch>
+                    {routes.map(props => {
+                        return <Route key={props.path as string} {...props} />;
+                    })}
+                    <Route path="*" component={() => <LayoutList fullScreen={true}><Column fill={true}>404. Page not found.</Column></LayoutList>}/>
+                </Switch>
+            </ConnectedRouter>
+        </Provider>
     );
 };
--- src/common/store/configureStore.ts
+++ src/common/store/configureStore.ts
@@ -1,6 +1,8 @@
 import { routerMiddleware } from 'connected-react-router';
 import { History } from 'history';
 import { applyMiddleware, createStore, Store } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 import { rootReducer } from './rootReducer';
 import { ApplicationState } from './ApplicationState';
@@ -9,6 +11,6 @@ export function configureStore(history: History): Store<ApplicationState> {
     const historyMiddleware = routerMiddleware(history);
     const middleware = [historyMiddleware];
 
-    const store = createStore(rootReducer(history), applyMiddleware(...middleware));
+    const store = createStore(rootReducer(history), composeWithDevTools(applyMiddleware(...middleware)));
     return store;
 }
