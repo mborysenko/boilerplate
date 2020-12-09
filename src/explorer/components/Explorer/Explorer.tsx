e88b46b6b476fe81cccf18cb9ed4947c701a5bfe
Maksym Borysenko <borysenko.maksym@gmail.com>
2020-12-09 15:46:32 +0100

    Improved main layout

--- src/explorer/components/Explorer/Explorer.tsx
+++ src/explorer/components/Explorer/Explorer.tsx
@@ -9,18 +9,16 @@ interface ExplorerProps extends RouteComponentProps {
 const Explorer: React.FunctionComponent<ExplorerProps> = (props) => {
     const {
         location,
-        match,
     } = props;
     return (
         <LayoutList fullScreen={true} direction={LayoutListDirection.VERTICAL}>
-            <Row>Explorer</Row>
             {isAreaVisible(RoutingArea.HEADING, location.pathname) && <Row>
                 <Area area={RoutingArea.HEADING}/>
             </Row>}
             {isAreaVisible(RoutingArea.SUBHEADING, location.pathname) && <Row>
                 <Area area={RoutingArea.SUBHEADING}/>
             </Row>}
-            <Row greedy={true}>
+            <Row greedy={true} asGrid={true}>
                 {isAreaVisible(RoutingArea.LEFT, location.pathname) &&
                 <Column>
                     <Area area={RoutingArea.LEFT}/>
