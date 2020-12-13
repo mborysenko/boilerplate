1002f85698b54168adf26252976f7aebd3460903
Maksym Borysenko <mborysenko@sdl.com>
2020-12-13 21:45:58 +0100

    Changed Application entry point file, added global reset styles, moved React Router initial logic to the AppRoot component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,22 @@
 import * as React from 'react';
-import { ConnectedRouter } from 'connected-react-router';
-import { Redirect, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import { EnhancedRootRoute, getRoutingRegistry } from '@common/routing';
-import { history, store } from '@common/store';
+import { store } from '@common/store';
+import { AppRoot } from '@common/components/dom/AppRoot/AppRoot';
 
-//Routing
-import '@explorer/routing';
-import { RouteProps } from 'react-router';
+
+/* Styles */
+import './styles.less';
+
+/* Routing */
+import './implementation/routing';
 
 export type ApplicationProps = {};
 
 export const App: React.FunctionComponent<ApplicationProps> = () => {
-    const routes = getRoutingRegistry();
-
     return (
         <Provider store={store}>
-            <ConnectedRouter history={history}>
-                <Switch>
-                    {routes.map(({ path, redirect, component, children, render , exact}) => {
-                        const relevant: RouteProps = {
-                            component,
-                            render,
-                            children,
-                            exact,
-                        };
-                        const id = Array.isArray(path) ? path.join('_') : path;
-
-                        return redirect
-                            ? <Route key={id} exact={exact} path={path}>
-                                <Redirect to={redirect!}/>
-                            </Route>
-                            : <Route key={id} path={path} {...relevant} />
-                    })}
-                </Switch>
-            </ConnectedRouter>
+            <AppRoot/>
         </Provider>
     );
 };
---
+++ src/common/components/dom/AppRoot/AppRoot.tsx
@@ -0,0 +1,30 @@
+import { FunctionComponent, HTMLAttributes } from 'react';
+import { RouteProps } from 'react-router';
+import { getRoutingRegistry, history } from '@common';
+import { ConnectedRouter } from 'connected-react-router';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import * as React from 'react';
+import { AppRootProps } from '@common/components/dom/AppRoot/API';
+
+export const AppRoot: FunctionComponent<AppRootProps & RouteProps & HTMLAttributes<any>> = () => {
+    const routes = getRoutingRegistry();
+    return <ConnectedRouter history={history}>
+        <Switch>
+            {routes.map(({ path, redirect, component, children, render , exact}) => {
+                const relevant: RouteProps = {
+                    component,
+                    render,
+                    children,
+                    exact,
+                };
+                const id = Array.isArray(path) ? path.join('_') : path;
+
+                return redirect
+                    ? <Route key={id} exact={exact} path={path}>
+                        <Redirect to={redirect!}/>
+                    </Route>
+                    : <Route key={id} path={path} {...relevant} />
+            })}
+        </Switch>
+    </ConnectedRouter>
+};
