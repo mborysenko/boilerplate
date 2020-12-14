c3764be52aaa87b7c3be3d79cd1948dd77818023
Maksym Borysenko <mborysenko@sdl.com>
2020-12-14 22:02:18 +0100

    Improved logic that checks if route exists

--- src/common/routing/utils/registerRootRouting.ts
+++ src/common/routing/utils/registerRootRouting.ts
@@ -3,8 +3,8 @@ import { EnhancedRootRoute, registry } from '../factory';
 const registerRootRouting: (routes: EnhancedRootRoute[]) => void = (routing = []) => {
     routing.forEach(rootRoute => {
         const { id: rootRouteId } = rootRoute;
-        const pile = registry.find(entry => entry.id === rootRouteId);
-        if (pile) {
+        const exists = registry.some(entry => entry.id === rootRouteId);
+        if (exists) {
             throw Error(`Root routing with ID ${rootRouteId} already exists. Please, choose another id`);
         } else {
             registry.push(rootRoute);
