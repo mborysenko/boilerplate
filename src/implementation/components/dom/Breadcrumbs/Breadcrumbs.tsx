e72fff1959ca007d46fe52e6ab201b794c5c574e
Maksym Borysenko <mborysenko@sdl.com>
2020-12-20 21:43:16 +0100

    Updated Breadcrumbs and Copyright components with Reducer

--- src/implementation/components/dom/Breadcrumbs/Breadcrumbs.tsx
+++ src/implementation/components/dom/Breadcrumbs/Breadcrumbs.tsx
@@ -2,10 +2,15 @@ import React, { FunctionComponent, HTMLAttributes } from 'react';
 
 import styles from './styles.less';
 import { Box } from '@common';
+import { Reducer } from '@common/components';
 
 export const Breadcrumbs: FunctionComponent<{} & HTMLAttributes<HTMLDivElement>> = (props) => {
     const { breadcrumbs } = styles;
-    return <Box className={breadcrumbs}><div className={breadcrumbs}>
-        Breadcrumbs
-    </div></Box>
+    return <Reducer>
+        <Box className={breadcrumbs}>
+            <div className={breadcrumbs}>
+                Breadcrumbs
+            </div>
+        </Box>
+    </Reducer>;
 }
--- src/implementation/components/dom/Copyright/Copyright.tsx
+++ src/implementation/components/dom/Copyright/Copyright.tsx
@@ -2,10 +2,15 @@ import React, { FunctionComponent, HTMLAttributes } from 'react';
 
 import styles from './styles.less';
 import { Box } from '@common';
+import { Reducer } from '@common/components';
 
 export const Copyright: FunctionComponent<{} & HTMLAttributes<HTMLDivElement>> = (props) => {
     const { breadcrumbs } = styles;
-    return <Box className={breadcrumbs}><div className={breadcrumbs}>
-        Copyright
-    </div></Box>
+    return <Reducer>
+        <Box className={breadcrumbs}>
+            <div className={breadcrumbs}>
+                Copyright
+            </div>
+        </Box>
+    </Reducer>;
 }
