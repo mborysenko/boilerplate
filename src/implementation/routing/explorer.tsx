9b0325862716a623dd5b20cb06fd6f5bf94dbe4e
Maksym Borysenko <mborysenko@sdl.com>
2020-12-20 21:43:53 +0100

    Added long example text

--- src/implementation/routing/explorer.tsx
+++ src/implementation/routing/explorer.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 
 import { Navigation, registerRootRouting, RoutingArea } from '@common/routing';
 
-import { AppLayout } from '@common/components';
+import { AppLayout, Box, Column } from '@common/components';
 import { Footer, ErrorPage, Copyright, Breadcrumbs } from '../components/dom';
+import { Reducer } from '@common/components/dom/Reducer';
+import { Scroller } from '@common/components/dom/Scroller';
 
 registerRootRouting([
     {
@@ -17,19 +19,19 @@ registerRootRouting([
                     {
                         area: RoutingArea.SUBHEADING,
                         render: props => <Navigation items={[
-                                {
-                                    to: '/explorer/content/repo',
-                                    label: 'Repository',
-                                },
-                                {
-                                    to: '/explorer/content/publishing',
-                                    label: 'Publishing Queue',
-                                },
-                                {
-                                    to: '/explorer/content/translation',
-                                    label: 'Translation Jobs',
-                                },
-                            ]} {...props}/>,
+                            {
+                                to: '/explorer/content/repo',
+                                label: 'Repository',
+                            },
+                            {
+                                to: '/explorer/content/publishing',
+                                label: 'Publishing Queue',
+                            },
+                            {
+                                to: '/explorer/content/translation',
+                                label: 'Translation Jobs',
+                            },
+                        ]} {...props}/>,
                     },
                     {
                         exact: true,
@@ -41,7 +43,122 @@ registerRootRouting([
                     {
                         area: RoutingArea.MAIN,
                         render: props => {
-                            return <div>Content Section 1</div>;
+                            return <Column greedy={true} relative={true}><Scroller>
+                                <Reducer>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Nullam non leo nunc. Praesent in facilisis libero. Vestibulum vel mi ullamcorper, cursus libero in, congue turpis.
+                                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                        Integer sed odio pharetra, sagittis mauris quis, tincidunt nisl. Fusce luctus finibus interdum.
+                                        Etiam quis posuere nisi. Vivamus at viverra eros.
+                                        Proin nibh ante, bibendum ac libero eget, sollicitudin blandit dui.
+                                        Morbi vehicula velit quis diam luctus laoreet. Quisque vitae est ipsum.
+                                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+                                        Vivamus vitae tincidunt diam, non consequat est. Suspendisse in pellentesque nulla, quis luctus nunc.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis blandit urna sit amet eleifend.
+                                        Aliquam eget tincidunt tortor. In tempor tortor et nibh consectetur vestibulum.
+                                        Vivamus et congue tellus. Vestibulum quis tellus non nulla laoreet tincidunt vel ac magna.
+                                        Curabitur consectetur iaculis risus, a laoreet mauris pulvinar vel. Quisque at venenatis ipsum.
+                                        Aliquam nulla odio, fermentum in nunc sit amet, eleifend rhoncus neque. Sed auctor enim nec ornare iaculis.
+                                        Aenean scelerisque, diam et tempor vulputate, nisi orci sollicitudin odio, eu sollicitudin dolor tortor id arcu.
+                                        Nullam eget tempor nisl. Sed et bibendum enim, et porttitor nisi. Curabitur eget pellentesque nisl, ut posuere ex.
+                                        Proin consectetur pretium sem, dictum blandit lorem sagittis id. Aliquam consequat sapien semper, pellentesque est nec, dictum lectus.
+                                        Fusce eu mollis justo. Maecenas vestibulum mollis quam sed commodo. In fermentum ullamcorper vulputate.
+                                        Curabitur ultricies nulla vitae pulvinar pharetra.
+                                        Cras semper suscipit fermentum. Morbi venenatis luctus nulla sed posuere. Aliquam eu fermentum elit.
+                                        Sed sit amet porta lectus. Nunc id ipsum sit amet sapien pellentesque molestie.
+                                        Nam efficitur porttitor blandit. Etiam vestibulum elementum mauris molestie ultrices. Donec id nisi a nibh interdum cursus at et nisl.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Nullam non leo nunc. Praesent in facilisis libero. Vestibulum vel mi ullamcorper, cursus libero in, congue turpis.
+                                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                        Integer sed odio pharetra, sagittis mauris quis, tincidunt nisl. Fusce luctus finibus interdum.
+                                        Etiam quis posuere nisi. Vivamus at viverra eros.
+                                        Proin nibh ante, bibendum ac libero eget, sollicitudin blandit dui.
+                                        Morbi vehicula velit quis diam luctus laoreet. Quisque vitae est ipsum.
+                                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+                                        Vivamus vitae tincidunt diam, non consequat est. Suspendisse in pellentesque nulla, quis luctus nunc.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis blandit urna sit amet eleifend.
+                                        Aliquam eget tincidunt tortor. In tempor tortor et nibh consectetur vestibulum.
+                                        Vivamus et congue tellus. Vestibulum quis tellus non nulla laoreet tincidunt vel ac magna.
+                                        Curabitur consectetur iaculis risus, a laoreet mauris pulvinar vel. Quisque at venenatis ipsum.
+                                        Aliquam nulla odio, fermentum in nunc sit amet, eleifend rhoncus neque. Sed auctor enim nec ornare iaculis.
+                                        Aenean scelerisque, diam et tempor vulputate, nisi orci sollicitudin odio, eu sollicitudin dolor tortor id arcu.
+                                        Nullam eget tempor nisl. Sed et bibendum enim, et porttitor nisi. Curabitur eget pellentesque nisl, ut posuere ex.
+                                        Proin consectetur pretium sem, dictum blandit lorem sagittis id. Aliquam consequat sapien semper, pellentesque est nec, dictum lectus.
+                                        Fusce eu mollis justo. Maecenas vestibulum mollis quam sed commodo. In fermentum ullamcorper vulputate.
+                                        Curabitur ultricies nulla vitae pulvinar pharetra.
+                                        Cras semper suscipit fermentum. Morbi venenatis luctus nulla sed posuere. Aliquam eu fermentum elit.
+                                        Sed sit amet porta lectus. Nunc id ipsum sit amet sapien pellentesque molestie.
+                                        Nam efficitur porttitor blandit. Etiam vestibulum elementum mauris molestie ultrices. Donec id nisi a nibh interdum cursus at et nisl.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Nullam non leo nunc. Praesent in facilisis libero. Vestibulum vel mi ullamcorper, cursus libero in, congue turpis.
+                                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                        Integer sed odio pharetra, sagittis mauris quis, tincidunt nisl. Fusce luctus finibus interdum.
+                                        Etiam quis posuere nisi. Vivamus at viverra eros.
+                                        Proin nibh ante, bibendum ac libero eget, sollicitudin blandit dui.
+                                        Morbi vehicula velit quis diam luctus laoreet. Quisque vitae est ipsum.
+                                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+                                        Vivamus vitae tincidunt diam, non consequat est. Suspendisse in pellentesque nulla, quis luctus nunc.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis blandit urna sit amet eleifend.
+                                        Aliquam eget tincidunt tortor. In tempor tortor et nibh consectetur vestibulum.
+                                        Vivamus et congue tellus. Vestibulum quis tellus non nulla laoreet tincidunt vel ac magna.
+                                        Curabitur consectetur iaculis risus, a laoreet mauris pulvinar vel. Quisque at venenatis ipsum.
+                                        Aliquam nulla odio, fermentum in nunc sit amet, eleifend rhoncus neque. Sed auctor enim nec ornare iaculis.
+                                        Aenean scelerisque, diam et tempor vulputate, nisi orci sollicitudin odio, eu sollicitudin dolor tortor id arcu.
+                                        Nullam eget tempor nisl. Sed et bibendum enim, et porttitor nisi. Curabitur eget pellentesque nisl, ut posuere ex.
+                                        Proin consectetur pretium sem, dictum blandit lorem sagittis id. Aliquam consequat sapien semper, pellentesque est nec, dictum lectus.
+                                        Fusce eu mollis justo. Maecenas vestibulum mollis quam sed commodo. In fermentum ullamcorper vulputate.
+                                        Curabitur ultricies nulla vitae pulvinar pharetra.
+                                        Cras semper suscipit fermentum. Morbi venenatis luctus nulla sed posuere. Aliquam eu fermentum elit.
+                                        Sed sit amet porta lectus. Nunc id ipsum sit amet sapien pellentesque molestie.
+                                        Nam efficitur porttitor blandit. Etiam vestibulum elementum mauris molestie ultrices. Donec id nisi a nibh interdum cursus at et nisl.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Nullam non leo nunc. Praesent in facilisis libero. Vestibulum vel mi ullamcorper, cursus libero in, congue turpis.
+                                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                        Integer sed odio pharetra, sagittis mauris quis, tincidunt nisl. Fusce luctus finibus interdum.
+                                        Etiam quis posuere nisi. Vivamus at viverra eros.
+                                        Proin nibh ante, bibendum ac libero eget, sollicitudin blandit dui.
+                                        Morbi vehicula velit quis diam luctus laoreet. Quisque vitae est ipsum.
+                                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+                                        Vivamus vitae tincidunt diam, non consequat est. Suspendisse in pellentesque nulla, quis luctus nunc.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis blandit urna sit amet eleifend.
+                                        Aliquam eget tincidunt tortor. In tempor tortor et nibh consectetur vestibulum.
+                                        Vivamus et congue tellus. Vestibulum quis tellus non nulla laoreet tincidunt vel ac magna.
+                                        Curabitur consectetur iaculis risus, a laoreet mauris pulvinar vel. Quisque at venenatis ipsum.
+                                        Aliquam nulla odio, fermentum in nunc sit amet, eleifend rhoncus neque. Sed auctor enim nec ornare iaculis.
+                                        Aenean scelerisque, diam et tempor vulputate, nisi orci sollicitudin odio, eu sollicitudin dolor tortor id arcu.
+                                        Nullam eget tempor nisl. Sed et bibendum enim, et porttitor nisi. Curabitur eget pellentesque nisl, ut posuere ex.
+                                        Proin consectetur pretium sem, dictum blandit lorem sagittis id. Aliquam consequat sapien semper, pellentesque est nec, dictum lectus.
+                                        Fusce eu mollis justo. Maecenas vestibulum mollis quam sed commodo. In fermentum ullamcorper vulputate.
+                                        Curabitur ultricies nulla vitae pulvinar pharetra.
+                                        Cras semper suscipit fermentum. Morbi venenatis luctus nulla sed posuere. Aliquam eu fermentum elit.
+                                        Sed sit amet porta lectus. Nunc id ipsum sit amet sapien pellentesque molestie.
+                                        Nam efficitur porttitor blandit. Etiam vestibulum elementum mauris molestie ultrices. Donec id nisi a nibh interdum cursus at et nisl.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Nullam non leo nunc. Praesent in facilisis libero. Vestibulum vel mi ullamcorper, cursus libero in, congue turpis.
+                                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                        Integer sed odio pharetra, sagittis mauris quis, tincidunt nisl. Fusce luctus finibus interdum.
+                                        Etiam quis posuere nisi. Vivamus at viverra eros.
+                                        Proin nibh ante, bibendum ac libero eget, sollicitudin blandit dui.
+                                        Morbi vehicula velit quis diam luctus laoreet. Quisque vitae est ipsum.
+                                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+                                        Vivamus vitae tincidunt diam, non consequat est. Suspendisse in pellentesque nulla, quis luctus nunc.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis blandit urna sit amet eleifend.
+                                        Aliquam eget tincidunt tortor. In tempor tortor et nibh consectetur vestibulum.
+                                        Vivamus et congue tellus. Vestibulum quis tellus non nulla laoreet tincidunt vel ac magna.
+                                        Curabitur consectetur iaculis risus, a laoreet mauris pulvinar vel. Quisque at venenatis ipsum.
+                                        Aliquam nulla odio, fermentum in nunc sit amet, eleifend rhoncus neque. Sed auctor enim nec ornare iaculis.
+                                        Aenean scelerisque, diam et tempor vulputate, nisi orci sollicitudin odio, eu sollicitudin dolor tortor id arcu.
+                                        Nullam eget tempor nisl. Sed et bibendum enim, et porttitor nisi. Curabitur eget pellentesque nisl, ut posuere ex.
+                                        Proin consectetur pretium sem, dictum blandit lorem sagittis id. Aliquam consequat sapien semper, pellentesque est nec, dictum lectus.
+                                        Fusce eu mollis justo. Maecenas vestibulum mollis quam sed commodo. In fermentum ullamcorper vulputate.
+                                        Curabitur ultricies nulla vitae pulvinar pharetra.
+                                        Cras semper suscipit fermentum. Morbi venenatis luctus nulla sed posuere. Aliquam eu fermentum elit.
+                                        Sed sit amet porta lectus. Nunc id ipsum sit amet sapien pellentesque molestie.
+                                        Nam efficitur porttitor blandit. Etiam vestibulum elementum mauris molestie ultrices. Donec id nisi a nibh interdum cursus at et nisl.
+                                    </p>
+                                </Reducer>
+                            </Scroller></Column>;
                         },
                     },
                     {
