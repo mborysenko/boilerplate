f29c281ce3b43f80a3159cf545a715579cde8387
Maksym Borysenko <borysenko.maksym@gmail.com>
2020-12-30 09:46:29 +0100

    Improved redirect tag rendering

--- src/core/routing/dom/Area/Area.tsx
+++ src/core/routing/dom/Area/Area.tsx
@@ -1,47 +1,46 @@
 import * as React from 'react';
-import { Route, Redirect, Switch, RouteProps } from 'react-router';
+import {Route, Redirect, Switch, RouteProps} from 'react-router';
 
-import { EnhancedAreaRoute, RoutingArea, useRoutesForArea } from '../../../routing';
+import {EnhancedAreaRoute, RoutingArea, useRoutesForArea} from '../../../routing';
 
 interface AreaProps {
     area: RoutingArea;
 }
 
 const renderRedirect = (from: string, to: string) => {
-    return <Redirect key={`${from}-${to}`} exact={true} {...{ from, to }}/>
+    return <Route key={`${from}-${to}`} path={from}>
+        <Redirect exact={true} to={to}/>
+    </Route>;
 }
 
-const Area: React.FunctionComponent<AreaProps & RouteProps> = ({ area, location }) => {
+const Area: React.FunctionComponent<AreaProps & RouteProps> = ({area, location}) => {
     const routes: EnhancedAreaRoute[] = useRoutesForArea(area, location?.pathname!);
-    return (
-        <>
-            <Switch>
-            {routes.map(({
-                             path,
-                             exact,
-                             rendering = [],
-                             redirect
-                         }) => {
+    return <Switch>
+        {routes.map(({
+                         path,
+                         exact,
+                         rendering = [],
+                         redirect
+                     }) => {
 
-                if (rendering?.length === 0) {
-                    return;
-                }
+            if (rendering?.length === 0) {
+                return;
+            }
 
-                const { component, render, children } = rendering[0];
-                const props: RouteProps = {
-                    component,
-                    render,
-                    children,
-                };
+            const {component, render, children} = rendering[0];
+            const props: RouteProps = {
+                component,
+                render,
+                children,
+            };
 
-                const key = Array.isArray(path) ? path.join('_') : path
-                return (redirect && redirect !== '')
-                    ? renderRedirect(path as string, redirect)
-                    : <Route {...{ key, path, exact, ...props }} />;
-            })}
-            </Switch>
-        </>
-    );
+            const key = Array.isArray(path) ? path.join('_') : path
+            return <>
+                <Route {...{key, path, exact, ...props}} />
+                {(redirect && redirect !== '') && renderRedirect(path as string, redirect)}
+            </>;
+        })}
+    </Switch>;
 };
 
-export { Area };
+export {Area};
