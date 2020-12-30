696b80cc4aa658600dcd001ed22a704152872c9e
Maksym Borysenko <borysenko.maksym@gmail.com>
2020-12-30 09:59:40 +0100

    Did some peformance optimization, moved repository layout to the correct route

--- src/core/routing/hooks/useRoutesForArea.tsx
+++ src/core/routing/hooks/useRoutesForArea.tsx
@@ -1,15 +1,18 @@
 import { getRoutingRegistry } from './getRoutingRegistry';
 import { RoutingArea, EnhancedAreaRoute, flattenRoutes, RouteWithChildren } from '../..';
 import * as React from 'react';
+import {useMemo} from "react";
 
 const useRoutesForArea: (area: RoutingArea, currentPath: string) => EnhancedAreaRoute[] = (area, currentPath) => {
     const rootRoutes = getRoutingRegistry();
     const [areaRoutes, setAreaRoutes] = React.useState<EnhancedAreaRoute[]>([]);
+    const routes = useMemo(
+        () => flattenRoutes(rootRoutes, area, currentPath),
+        [rootRoutes, rootRoutes.length, area, currentPath])
 
     React.useEffect(() => {
-        let result: EnhancedAreaRoute[] = flattenRoutes(rootRoutes, area, currentPath);
-        setAreaRoutes(result);
-    }, [rootRoutes, area, currentPath]);
+        setAreaRoutes(routes);
+    }, [routes]);
 
     return areaRoutes;
 };
