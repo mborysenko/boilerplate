7cf14ffb55aff154d6721a212c6961ae3d38b925
Maksym Borysenko <mborysenko@sdl.com>
2021-02-05 21:12:39 +0100

    Tweaked eslint and stylelint jest components

--- jest/quality/stylelint.config.ts
+++ jest/quality/stylelint.config.ts
@@ -3,7 +3,7 @@ export { Config } from '@jest/types'
 const config = {
     runner: "jest-runner-stylelint",
     displayName: "lint:css",
-    rootDir: "../../",
+    rootDir: "../..",
     moduleFileExtensions: ["css"],
     testMatch: ["<rootDir>/src/**/*.css"]
 };
