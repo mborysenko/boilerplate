ad429b05830fbf13fa6f0fbdf802eedcb00c54bb
Maksym Borysenko <mborysenko@sdl.com>
2021-02-15 09:35:52 +0100

    Now visibility of Application Manager Panel can be toggled

--- src/core/application/manager/dom/AppNavigation/AppNavigation.tsx
+++ src/core/application/manager/dom/AppNavigation/AppNavigation.tsx
@@ -1,6 +1,8 @@
-import React, { FunctionComponent, ReactNode, useCallback } from 'react';
-import { Row } from '@core/components';
-import classnames from 'classnames'
+import React, { FunctionComponent, ReactNode, useCallback, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CSSTransition } from 'react-transition-group';
+import classnames from 'classnames';
 
 import styles from './styles.less';
 
@@ -22,9 +24,10 @@ export const AppNavigationItem: FunctionComponent<AppNavigationItemProps> = (pro
 
     const onClickInner = useCallback(() => {
         onClick?.(id);
+
     }, [id]);
 
-    return <span className={styles.item} onClick={onClickInner}>{label}</span>;
+    return <li className={styles.item} onClick={onClickInner}>{label}</li>;
 };
 
 export interface AppNavigationProps {
@@ -34,19 +37,43 @@ export interface AppNavigationProps {
 
 export const AppNavigation: FunctionComponent<AppNavigationProps> = (props) => {
     const { children } = props;
+
+    const [showPanel, setShowPanel] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const onCall = useCallback(() => {
+        setShowPanel(!showPanel);
+        setIsVisible(!isVisible);
+    }, [showPanel, isVisible]);
+
     const {
         appNav,
         appMenu,
         caller,
-        button,
     } = styles;
 
-    return <div className={appNav}>
-        <div className={caller}>
-            <a className={button}>Menu</a>
+    const panelClasses = classnames({
+        [styles.open]: showPanel,
+        [appNav]: true,
+    })
+    return <div className={panelClasses}>
+        <div className={caller} onClick={onCall}>
+            <FontAwesomeIcon icon={isVisible ? faTimes : faBars} size={'2x'}/>
         </div>
-        <Row className={appMenu} asGrid={true}>
-            {children}
-        </Row>
+        <CSSTransition
+            key={"dkfjgsdkfjh"}
+            unmountOnExit
+            timeout={{ enter: 500, exit: 200 }}
+            in={showPanel}
+            classNames={{
+                enter: styles.appNavEnter,
+                enterActive: styles.appNavEnterActive,
+                exitActive: styles.appNavExitActive,
+                exitDone: styles.appNavExit,
+            }}>
+            <ul className={appMenu}>
+                {children}
+            </ul>
+        </CSSTransition>
     </div>;
 };
