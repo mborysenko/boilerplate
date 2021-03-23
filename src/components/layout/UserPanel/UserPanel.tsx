6437c6821dcccf8c8ae24590f9be322ef035490e
Maksym Borysenko <mborysenko@sdl.com>
2021-03-23 18:26:58 +0100

    Added theme switcher to User Panel

--- src/components/layout/UserPanel/UserPanel.tsx
+++ src/components/layout/UserPanel/UserPanel.tsx
@@ -1,6 +1,31 @@
-import React, { FunctionComponent } from 'react';
-import { Column } from '@dhampir/core';
+import React, { FunctionComponent, useCallback, useContext } from 'react';
+import { BorderSide, Decorator, Label, Row, SkinContext, SkinContextValue, Spacer, Units, useThemeList } from '@dhampir/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBicycle } from '@fortawesome/free-solid-svg-icons';
 
 export const UserPanel: FunctionComponent = () => {
-    return <Column greedy={true}>User Panel</Column>
+    const { defaultThemeId, setTheme } = useContext<SkinContextValue>(SkinContext);
+
+    const themes = useThemeList();
+
+    const onClick = useCallback((themeId) => {
+        setTheme?.(themeId);
+    }, [setTheme]);
+
+    return <Spacer space={0.5} units={Units.EM}>
+        <Row alignItems={'center'}>
+            <Label>Theme:</Label>
+            {themes.map(theme => {
+                const {
+                    id,
+                    title,
+                } = theme;
+                const isDefault = defaultThemeId === id;
+
+                return <Decorator key={id} {...(isDefault ? { borderPosition: [BorderSide.ALL_OVER], borderColor: '#ccc'} : {})} style={{ marginLeft: '0.5em'}}>
+                    <Spacer space={0.2} units={Units.EM}><FontAwesomeIcon size={'lg'} title={title} icon={faBicycle} onClick={() => onClick(id)}/></Spacer>
+                </Decorator>
+            })}
+        </Row>
+    </Spacer>;
 };
