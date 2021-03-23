2c1b3303ece0feadb87d62fa99388d6548976641
Maksym Borysenko <mborysenko@sdl.com>
2021-03-23 20:31:43 +0100

    Tweaked styles of the MainMenu

--- src/components/widgets/MainMenu.tsx
+++ src/components/widgets/MainMenu.tsx
@@ -1,9 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import { RouteChildrenProps } from 'react-router';
-import { NavDataProvider, Navigation } from '@dhampir/core';
+import { NavDataProvider, Navigation, Units, Spacer } from '@dhampir/core';
 
 export const MainMenu: FunctionComponent<{} & RouteChildrenProps<{}>> = (props) => {
-    return <NavDataProvider isRoot={true} {...props}>
-        <Navigation {...props} />
-    </NavDataProvider>;
+    return <Spacer greedy={true} space={0.5} units={Units.EM}>
+        <NavDataProvider isRoot={true} {...props}>
+            <Navigation {...props} />
+        </NavDataProvider>
+    </Spacer>;
 };
