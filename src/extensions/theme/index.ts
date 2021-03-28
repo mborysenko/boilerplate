af4ace31f859b16a71e9a3c0c4a3177ff39019d3
Maksym Borysenko <mborysenko@sdl.com>
2021-03-28 12:05:28 +0200

    Added custom theme

---
+++ src/extensions/theme/index.ts
@@ -0,0 +1,9 @@
+import { registerTheme } from '@dhampir/core';
+import { colors } from './colors';
+
+registerTheme({
+    id: 'bolilerPlateTheme',
+    default: false,
+    title: 'Boilerplate Theme',
+    colors,
+}, true);
