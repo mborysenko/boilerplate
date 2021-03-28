48cc94eed130bd1e40ae975f67df5f0f18ab7abc
Maksym Borysenko <mborysenko@sdl.com>
2021-03-28 11:39:11 +0200

    Added eslint task to find and fix issues with the code

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,7 @@ import clean from 'gulp-clean';
 import bundle from './build/gulp-tasks/bundle';
 import start from './build/gulp-tasks/serve';
 import jest from './build/gulp-tasks/jest';
+import eslint from './build/gulp-tasks/eslint';
 
 gulp.task("clean", (done) => {
     const { dist, filesToClean } = configuration;
@@ -13,7 +14,8 @@ gulp.task("clean", (done) => {
     fs.removeSync(dist);
 
     return gulp.src(filesToClean)
-        .pipe(clean());
+        .pipe(clean())
+        .on('end', done);
 });
 
 gulp.task("build", (done) => {
@@ -28,7 +30,14 @@ gulp.task("test", (done) => {
     jest(configuration)(done);
 });
 
-gulp.task("default", gulp.series("clean", "build", (done) => done()));
+gulp.task('eslint', (cb) => {
+    eslint(configuration)(cb);
+});
+gulp.task('eslint:fix', (cb) => {
+    eslint(configuration, true)(cb);
+});
+
+gulp.task("default", gulp.series("clean", "eslint", "build", (done) => done()));
 
 gulp.task("start", gulp.series("default", "serve"), (done) => done());
 
